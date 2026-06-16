c3f4125d02b20cf71da865cb7211548e77db1e7f
Jessie A <jasherworks@gmail.com>
2026-06-16 12:40:10 -0700

    promises also have a catch if there's an error, if the service is down for some reason

--- index.js
+++ index.js
@@ -4,24 +4,30 @@
 
 // 
 
-// function contact(event) {
-//     event.preventDefault();
-//     emailjs
-//         .sendForm(
-//             'service_g6f3t9u',
-//             'template_stts2vc',
-//             event.target,
-//             '1WE98dJnq0vWLreyt'
-//         ).then() ==> {
-//             console.log('this worked1')
-//         }
-// })
-const loading = document.querySelector('.modal__overlay--loading')
-const success = document.querySelector('.modal__overlay--success')
+function contact(event) {
+    event.preventDefault();
+    const loading = document.querySelector('.modal__overlay--loading')
+    const success = document.querySelector('.modal__overlay--success')
+
+    emailjs
+        .sendForm(
+            'service_g6f3t9u',
+            'template_stts2vc',
+            event.target,
+            '1WE98dJnq0vWLreyt'
+        ).then() ==> {
+            loading.classList.remove("modal__overlay--visible")
+            success.classList += " modal__overlay--visible"
+        }
+}).catch(() => {
+    
+})
+
+
+
 loading.classList += " modal__overlay--visible"
 setTimeout(() => {
-    loading.classList.remove("modal__overlay--visible")
-    success.classList += " modal__overlay--visible"
+   
     console.log('it worked 1')
 }, 1000);
 
