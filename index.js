b87664c5bf2961897867e37906fa492bf2c4f651
Jessie A <jasherworks@gmail.com>
2026-06-16 11:48:03 -0700

    add the ids in the arguments

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-// template_stts2vc
+// 
 
-// service_g6f3t9u
+// 
 
 // 1WE98dJnq0vWLreyt
 
 function contact(event) {
     event.preventDefault();
-    console.log('it worked')
+    emailjs
+        .sendForm(
+            'service_g6f3t9u'
+            'template_stts2vc'
+        )
 }
