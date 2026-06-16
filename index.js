a7a41368bd47b4c90b44fafa7dd0436d559e1156
Jessie A <jasherworks@gmail.com>
2026-06-16 11:49:50 -0700

    pass in user id

--- index.js
+++ index.js
@@ -2,14 +2,15 @@
 
 // 
 
-// 1WE98dJnq0vWLreyt
+// 
 
 function contact(event) {
     event.preventDefault();
     emailjs
         .sendForm(
-            'service_g6f3t9u'
-            'template_stts2vc'
-            event.target
+            'service_g6f3t9u',
+            'template_stts2vc',
+            event.target,
+            '1WE98dJnq0vWLreyt'
         )
 }
