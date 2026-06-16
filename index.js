a3b60b584a83e1d562aa3237b8695656f661259a
Jessie A <jasherworks@gmail.com>
2026-06-16 12:10:34 -0700

    now we want to show the success state because it successfully worked

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ const success = document.querySelector('.modal__overlay--success')
 loading.classList += " modal__overlay--visible"
 setTimeout(() => {
     loading.classList.remove("modal__overlay--visible");
+    success.classList += " modal__overlay--visible";
     console.log('it worked 1')
 }, 1000);
 
