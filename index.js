b5f3963d6f257c43f144a52d193bffeb7c488d36
Jessie A <jasherworks@gmail.com>
2026-06-16 13:30:18 -0700

    add java functionality for modal form

--- index.js
+++ index.js
@@ -8,27 +8,20 @@ function contact(event) {
     event.preventDefault();
     const loading = document.querySelector('.modal__overlay--loading')
     const success = document.querySelector('.modal__overlay--success')
-
+    loading.classList += " modal__overlay--visible"
     emailjs
         .sendForm(
             'service_g6f3t9u',
             'template_stts2vc',
             event.target,
             '1WE98dJnq0vWLreyt'
-        ).then() ==> {
-            loading.classList.remove("modal__overlay--visible")
-            success.classList += " modal__overlay--visible"
-        }
-}).catch(() => {
+    ).then() ==> {
+        loading.classList.remove("modal__overlay--visible")
+        success.classList += " modal__overlay--visible"
+    }).catch(() => {
     loading.classList.remove("modal__overlay--visible")
+    alert(
+        "The email service is temporarily unavailable. Please contact me directly on jasherworks@gmail.com"
+    )
 })
-
-
-
-loading.classList += " modal__overlay--visible"
-setTimeout(() => {
-   
-    console.log('it worked 1')
-}, 1000);
-
 }
