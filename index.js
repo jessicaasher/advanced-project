8790f543640aa01391a64d7480c57c3729e09d53
Jessie A <jasherworks@gmail.com>
2026-06-16 12:09:12 -0700

    after the timeout, we don't want the class anymore. we dont need a space here, we just target the class

--- index.js
+++ index.js
@@ -20,7 +20,8 @@ const loading = document.querySelector('.modal__overlay--loading')
 const success = document.querySelector('.modal__overlay--success')
 loading.classList += " modal__overlay--visible"
 setTimeout(() => {
+    loading.classList.remove("modal__overlay--visible")
     console.log('it worked 1')
-}, 500)
+}, 1000);
 
 }
