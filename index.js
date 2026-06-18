b284a26f64fda39c59e7cfb38366921db5775a2e
Jessie A <jasherworks@gmail.com>
2026-06-18 14:52:11 -0700

    fixing html and mouse movement code

--- index.js
+++ index.js
@@ -3,56 +3,55 @@ let contrastToggle = false;
 const scaleFactor = 1 / 20;
 
 function moveBackground(event) {
-    const shapes = document.querySelectorAll(".shape");
-    const x = event.clientX * scaleFactor;
-    const y = event.clientY * scaleFactor;
-   
-    for (let i = 0; i < shapes.length; ++i) {
-        const isOdd = i % 2 !== 0;
-        const boolInt = isOdd ? -1 : 1;
-        shapes[i].style.transform = 'translate(${x}px, $1{y}px)'
+  const shapes = document.querySelectorAll(".shape");
+  const x = event.clientX * scaleFactor;
+  const y = event.clientY * scaleFactor;
 
-    }
+  for (let i = 0; i < shapes.length; ++i) {
+    const isOdd = i % 2 !== 0;
+    const boolInt = isOdd ? -1 : 1;
+    shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`;
+  }
 }
 
 function toggleContrast() {
-    contrastToggle = !contrastToggle;
-    if (contrastToggle) {
-        document.body.classList += " dark-theme"
-    }
-    else {
-        document.body.classList.remove("dark-theme");
-    }
+  contrastToggle = !contrastToggle;
+  if (contrastToggle) {
+    document.body.classList += " dark-theme";
+  } else {
+    document.body.classList.remove("dark-theme");
+  }
 }
 
 function contact(event) {
-    event.preventDefault();
-    const loading = document.querySelector('.modal__overlay--loading')
-    const success = document.querySelector('.modal__overlay--success')
-    loading.classList += " modal__overlay--visible"
-    emailjs
-        .sendForm(
-            'service_g6f3t9u',
-            'template_stts2vc',
-            event.target,
-            '1WE98dJnq0vWLreyt'
-    ).then(() => {
-        loading.classList.remove("modal__overlay--visible")
-        success.classList += " modal__overlay--visible"
-    }).catch(() => {
-    loading.classList.remove("modal__overlay--visible")
-    alert(
-        "The email service is temporarily unavailable. Please contact me directly on jasherworks@gmail.com"
+  event.preventDefault();
+  const loading = document.querySelector(".modal__overlay--loading");
+  const success = document.querySelector(".modal__overlay--success");
+  loading.classList += " modal__overlay--visible";
+  emailjs
+    .sendForm(
+      "service_g6f3t9u",
+      "template_stts2vc",
+      event.target,
+      "1WE98dJnq0vWLreyt",
     )
-})  
+    .then(() => {
+      loading.classList.remove("modal__overlay--visible");
+      success.classList += " modal__overlay--visible";
+    })
+    .catch(() => {
+      loading.classList.remove("modal__overlay--visible");
+      alert(
+        "The email service is temporarily unavailable. Please contact me directly on jasherworks@gmail.com",
+      );
+    });
 }
 
-function toggleModal () {
-    if (isModalOpen) {
-        isModalOpen = false;
-        return document.body.classList.remove("modal--open");
-    }
-    isModalOpen = true;
-    document.body.classList += " modal--open"
-
+function toggleModal() {
+  if (isModalOpen) {
+    isModalOpen = false;
+    return document.body.classList.remove("modal--open");
+  }
+  isModalOpen = true;
+  document.body.classList += " modal--open";
 }
